44d077bdac6bc2aee56c0dc4a0e45e7a6097e3b5
Bakiury <thecutaseta@gmail.com>
2022-05-12 17:05:28 -0500

    Login and Register view were designed

---
+++ src/modules/auth/views/Login.vue
@@ -0,0 +1,135 @@
+<template>
+    <div class="all">
+        <div class="main-section">
+            <div class="modal-content">
+                <div class="user-img">
+                    <img src="@/assets/user.png" />
+                </div>
+                <form class="" autocomplete="off">
+                    <div class="form-group" id="user-group">
+                        <div>
+                            <i class="fas fa-user"></i>
+                        </div>
+                        <input
+                            id="myUser"
+                            type="text"
+                            class="form-control"
+                            placeholder="Email"
+                            name="username"
+                            ref="myUser"
+                        />
+                    </div>
+                    <div class="form-group">
+                        <div>
+                            <i class="fas fa-lock"></i>
+                        </div>
+                        <input
+                            id="myPass"
+                            type="password"
+                            class="form-control"
+                            placeholder="Contraseña"
+                            name="password"
+                        />
+                    </div>
+                    <button type="submit" class="btn">
+                        <i class="fas fa-sign-in-alt"></i> Ingresar
+                    </button>
+                </form>
+                <div class="col-12 forgot">
+                    <router-link :to="{ name: 'register' }" class="nav-link active"
+                        >No tienes una cuenta registrate aqui</router-link
+                    >
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'Login',
+})
+</script>
+
+<style scoped>
+.all {
+    position: absolute;
+    background-image: url('@/assets/background.jpg');
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
+.main-section {
+    position: absolute;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    top: 50%;
+    text-align: center;
+}
+.modal-content {
+    background-color: rgba(255, 234, 167, 0.8);
+    padding: 0 56px;
+    width: 385px;
+    box-shadow: 0px 0px 2px 3px #ffffff;
+    border-radius: 20px;
+}
+.user-img {
+    margin-top: -50px;
+    margin-bottom: 35px;
+}
+.user-img img {
+    width: 100px;
+    height: 100px;
+    box-shadow: 0px 0px 2px 3px #ffffff;
+    border-radius: 50%;
+}
+.form-group input {
+    height: 42px;
+    font-size: 18px;
+    border: 0;
+    padding: 25px;
+    padding-left: 60px;
+    border-radius: 5px;
+    color: #34495e;
+    font-weight: bold;
+    margin-bottom: 25px;
+    background-color: #ffffff;
+}
+.form-group div {
+    background: #333333;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    color: #ffffff;
+}
+button {
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    background: #333333;
+    color: #ffffff;
+}
+button:hover {
+    color: #ffffff;
+    background: #444444;
+}
+.forgot {
+    padding: 5px 0;
+}
+.forgot a {
+    font-size: 14px;
+    font-style: italic;
+    font-weight: bold;
+    color: #333333;
+}
+.forgot a:hover {
+    color: #444444;
+}
+</style>
---
+++ src/modules/auth/views/Register.vue
@@ -0,0 +1,192 @@
+<template>
+    <section class="contact-box">
+        <div class="myRegister">
+            <div class="myBox text-center">
+                <img src="@/assets/user.png" alt="logo" />
+            </div>
+
+            <div>
+                <div class="container align-self-center p-6">
+                    <h1 class="font-weight-bold mb-3 text-center">
+                        Crea tu cuenta
+                    </h1>
+                    <p class="text-muted mb-2 text-center">
+                        Ingresa la siguiente información para registrarte.
+                    </p>
+
+                    <form>
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="form-group mb-3">
+                                    <label class="font-weight-bold">
+                                        Nombre
+                                        <span class="text-danger">*</span>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="Ingrese su nombre"
+                                    />
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label class="font-weight-bold">
+                                        Apellido
+                                        <span class="text-danger">*</span>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="Ingrese su apellido"
+                                    />
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label class="font-weight-bold">
+                                        Email
+                                        <span class="text-danger">*</span>
+                                    </label>
+                                    <input
+                                        type="email"
+                                        class="form-control"
+                                        placeholder="Ingrese su email"
+                                    />
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label class="font-weight-bold">
+                                        Contraseña
+                                        <span class="text-danger">*</span>
+                                    </label>
+                                    <input
+                                        type="password"
+                                        class="form-control"
+                                        placeholder="Ingrese su contraseña"
+                                    />
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                <div class="form-group mb-3">
+                                    <label class="font-weight-bold">
+                                        Foto Perfil
+                                        <span class="text-danger">*</span>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="Ingrese su foto de perfil"
+                                    />
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label class="font-weight-bold">
+                                        Fecha de nacimiento
+                                        <span class="text-danger">*</span>
+                                    </label>
+                                    <input
+                                        type="date"
+                                        class="form-control"
+                                        placeholder="Ingrese su fecha de nacimiento"
+                                    />
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <label class="font-weight-bold">
+                                        Descripción
+                                        <span class="text-danger">*</span>
+                                    </label>
+                                    <textarea
+                                        cols="30"
+                                        rows="4"
+                                        class="form-control mt-2"
+                                        placeholder="Ingrese su descripción"
+                                    ></textarea>
+                                </div>
+                            </div>
+
+                            <br />
+
+                            <div class="form-group mb-5">
+                                <div
+                                    class="form-check d-flex justify-content-center"
+                                >
+                                    <input
+                                        class="form-check-input mx-2"
+                                        type="checkbox"
+                                    />
+                                    <label class="form-check-label text-muted">
+                                        Al seleccionar esta casilla aceptas
+                                        nuestro aviso de privacidad y los
+                                        términos y condiciones
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="d-flex">
+                                <button class="btn btn-primary">
+                                    <router-link
+                                        :to="{ name: 'login' }"
+                                        class="nav-link text-white"
+                                        >Volver</router-link
+                                    >
+                                </button>
+                                <button class="btn btn-success" type="submit">
+                                    Regístrate
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                    <small class="d-inline-block text-muted mt-5"
+                        >Todos los derechos reservados | © 2022 Bakiury</small
+                    >
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'Register',
+})
+</script>
+<style scoped>
+.contact-box {
+    position: absolute;
+    background-image: url('@/assets/background.jpg');
+    background-size: cover;
+    width: 100%;
+}
+.myRegister {
+    padding: 50px;
+    background-color: rgba(255, 234, 167, 0.8) !important;
+}
+.text-muted {
+    color: #555555 !important;
+}
+.myBox {
+    width: 200px;
+    height: 200px;
+    margin: 0 auto;
+}
+.myBox img {
+    width: 100%;
+    height: 100%;
+    box-shadow: 0px 0px 2px 3px #ffffff;
+    border-radius: 50%;
+}
+button {
+    margin: 0 auto;
+    width: 400px;
+}
+small {
+    position: relative;
+    transform: translate(-50%);
+    left: 50%;
+}
+.font-weight-bold {
+    font-weight: bold;
+}
+</style>
